bf86ba9e3b8db571ef27bf96ed9f7b89c0159906
Aileen Rae <aileen@stormideas.com>
2020-04-17 13:57:32 +0100

    Add test admin account

--- AspNetCoreTodo/Program.cs
+++ AspNetCoreTodo/Program.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +10,9 @@ namespace AspNetCoreTodo
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            InitializeDataBase(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +21,23 @@ namespace AspNetCoreTodo
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void InitializeDataBase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.InitializeAsync(services).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+        }
     }
 }
